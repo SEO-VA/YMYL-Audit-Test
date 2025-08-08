3a3c164ab7956b2ef8fa7b887530cd5511901523
SEO-VA <162134294+SEO-VA@users.noreply.github.com>
2025-08-08 14:04:42 +0200

    Delete processors/__init__.py

--- processors/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Content processing module for YMYL Audit Tool
-Chunk processing and browser automation
-"""
-
-from .chunk_processor import ChunkProcessor
-
-__all__ = ['ChunkProcessor']

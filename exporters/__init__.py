98dc7b240d9ba4267837bc1fcc6e89d14602efb6
SEO-VA <162134294+SEO-VA@users.noreply.github.com>
2025-08-08 14:06:07 +0200

    Delete exporters/__init__.py

--- exporters/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Content extraction module for YMYL Audit Tool
-Web scraping and content extraction functionality
-"""
-
-from .content_extractor import ContentExtractor
-
-__all__ = ['ContentExtractor']
